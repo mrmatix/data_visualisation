66241dc5fb2953982543c9e13a47bd229039a098
Mateusz Mocko <mati.mocko10@gmail.com>
2021-11-01 14:52:05 +0000

    fixed location

--- world_population/world_population.py
+++ world_population/world_population.py
@@ -38,4 +38,4 @@ wm.title = "World Population in 2010 by Country"
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn', cc_pops_2)
 wm.add('1bn+', cc_pops_3)
-wm.render_to_file('world_population.svg')
+wm.render_to_file('world_population/world_population.svg')
